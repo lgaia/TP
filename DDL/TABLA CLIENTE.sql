aa8503662794466815eabbf73e66186db9df07e3
lgaia <112736144+lgaia@users.noreply.github.com>
2022-09-04 12:48:49 -0300

    upload
    Tabla cliente

---
+++ DDL/TABLA CLIENTE.sql
@@ -0,0 +1,12 @@
+use bdd2;
+insert into cliente (id_cliente, costos, facturacion)
+values (1,1000,1500);
+insert into cliente (costos, facturacion)
+values (500,1000);
+insert into cliente (costos, facturacion)
+values (750,1000);
+insert into cliente (costos, facturacion)
+values (250,500);
+insert into cliente (costos, facturacion)
+values (1000,2000);
+
