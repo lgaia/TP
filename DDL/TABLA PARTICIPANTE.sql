9cbc60de5fc842816c06560dc11eb0cfb44eb0f0
lgaia <112736144+lgaia@users.noreply.github.com>
2022-09-04 14:57:07 -0300

    upload
    tabla participante

---
+++ DDL/TABLA PARTICIPANTE.sql
@@ -0,0 +1,23 @@
+use bdd2;
+insert into participante (id_legajo, hora_max, rol, id_proyecto,nombre_apellido)
+values (1, 100, "admin", 1, "juan gomez");
+insert into participante (hora_max, rol, id_proyecto, nombre_apellido)
+values (160, "manager", 2, "carlos perez");
+insert into participante (hora_max, rol, id_proyecto, nombre_apellido)
+values (200, "desarrolladora", 3, "julieta fernandez");
+insert into participante (hora_max, rol, id_proyecto, nombre_apellido)
+values (80, "manager", 4, "ana maria gutierrez");
+insert into participante (hora_max, rol, id_proyecto, nombre_apellido)
+values (220, "desarrollador", 5, "roberto mendez");
+insert into participante (hora_max, rol, id_proyecto, nombre_apellido)
+values (80, "ceo", 6, "carolina suarez");
+insert into participante (hora_max, rol, id_proyecto, nombre_apellido)
+values (70, "programador", 7, "marcelo alvarez");
+insert into participante (hora_max, rol, id_proyecto, nombre_apellido)
+values (100, "admin", 8, "marianela martinez");
+insert into participante (hora_max, rol, id_proyecto, nombre_apellido)
+values (180, "desarrolladora", 9, "selena martinelli");
+insert into participante (hora_max, rol, id_proyecto, nombre_apellido)
+values (40, "manager", 10, "alberto crosse");
+select * from participante;
+
