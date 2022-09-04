12a01f8bd8eb431f058f33bf44be4c8f165aed1a
lgaia <112736144+lgaia@users.noreply.github.com>
2022-09-04 13:08:04 -0300

    upload
    Tabla proyecto

---
+++ DDL/TABLA PROYECTO.sql
@@ -0,0 +1,22 @@
+use bdd2;
+insert into proyecto (id_proyecto, nombre_proyecto, id_cliente, hora_mensual)
+values(1, "Proyecto A", 1, 240);
+insert into proyecto (nombre_proyecto, id_cliente, hora_mensual)
+values("Proyecto B", 2, 120);
+insert into proyecto (nombre_proyecto, id_cliente, hora_mensual)
+values("Proyecto C", 3, 200);
+insert into proyecto (nombre_proyecto, id_cliente, hora_mensual)
+values("Proyecto D", 4, 60);
+insert into proyecto (nombre_proyecto, id_cliente, hora_mensual)
+values("Proyecto E", 5, 160);
+insert into proyecto (nombre_proyecto, id_cliente, hora_mensual)
+values("Proyecto F", 6, 220);
+insert into proyecto (nombre_proyecto, id_cliente, hora_mensual)
+values("Proyecto G", 7, 80);
+insert into proyecto (nombre_proyecto, id_cliente, hora_mensual)
+values("Proyecto H", 8, 140);
+insert into proyecto (nombre_proyecto, id_cliente, hora_mensual)
+values("Proyecto I", 9, 40);
+insert into proyecto (nombre_proyecto, id_cliente, hora_mensual)
+values("Proyecto J", 10, 100);
+select * from proyecto;
