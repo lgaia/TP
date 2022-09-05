ede94c1146eb9b090661a2d2695bfcf0203af71e
lgaia <112736144+lgaia@users.noreply.github.com>
2022-09-05 20:28:21 -0300

    Update Tablas.sql

--- DDL/Tablas.sql
+++ DDL/Tablas.sql
@@ -4,12 +4,13 @@ create table cliente (
 id_cliente int not null auto_increment primary key,
 costo int,
 facturacion int,
-horas_mensual
+horas_mensual int
 );
 create table proyecto (
 id_proyecto int not null auto_increment primary key,
 nombre_proyecto varchar (255),
 id_cliente int not null,
+hora_proyecto int,
 constraint fk_proyecto_cliente foreign key id_cliente references cliente(id_cliente)
 );
 create table participante (
@@ -20,5 +21,11 @@ rol varchar(255),
 id_proyecto int,
 fecha datetime,
 constraint fk_participante_proyecto foreign key id_proyecto references proyecto(id_proyecto)
+);
+create table liquidacion (
+  horas_proyecto int,
+  horas_cliente,
+  constraint fk_liquidacion_proyecto foreign key horas_proyecto references proyecto(hora_proyecto),
+  constraint fk_liquidacion_cliente foreign key horas_cliente references cliente(horas_mensual)
 );
 
