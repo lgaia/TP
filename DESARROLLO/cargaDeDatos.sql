050c974372fcd02341bf9013fb694899b67bf6b5
lgaia <112736144+lgaia@users.noreply.github.com>
2022-09-05 20:21:50 -0300

    Update cargaDeDatos.sql

--- DESARROLLO/cargaDeDatos.sql
+++ DESARROLLO/cargaDeDatos.sql
@@ -1,13 +1,17 @@
 DELIMITER $$
-CREATE PROCEDURE cargaDeDatos(IN 'idlegajo' int, IN 'nombreapellido' varchar,IN 'idproyecto' int, IN 'rol' varchar, IN 'horasdiarias' int, IN 'fecha' date)
+CREATE PROCEDURE cargaDeDatos(IN idlegajo int, IN nombreapellido varchar(150),IN idproyecto int, IN rol varchar(150), IN horasdiarias int, IN fecha date, OUT respuesta varchar(100))
 begin
 insert into participante (id_legajo, nombre_apellido, id_proyecto, rol)
 values (@idlegajo,@nombreapellido,@idproyecto,@rol);
 if(@horasdiarias<=120) THEN
+begin
 update table participante
 set horas_diarias=@horasdiarias
 and fecha=@fecha
 where id_legajo=@idlegajo
+end
+else
+set @respuesta='Las horas diarias cargadas exceden las posibles')
 END IF
 end
 $$
