8a46fb1a608bd616546826d2af9dba23c5b2921b
lgaia <112736144+lgaia@users.noreply.github.com>
2022-09-05 10:02:02 -0300

    Update TABLA PARTICIPANTE.sql

--- DDL/TABLA PARTICIPANTE.sql
+++ DDL/TABLA PARTICIPANTE.sql
@@ -1,25 +1,25 @@
 use bdd2;
-insert into participante (id_legajo, nombre_apellido, id_proyecto)
-values (1,"juan gomez",1);
-insert into participante (id_legajo, nombre_apellido, id_proyecto)
-values (2,"marcelo centeno",2);
-insert into participante (id_legajo, nombre_apellido, id_proyecto)
-values (3,"roberto alvarez",3);
-insert into participante (id_legajo, nombre_apellido, id_proyecto)
-values (4,"clara perez",4);
-insert into participante (id_legajo, nombre_apellido, id_proyecto)
-values (5,"ana maria gutierrez",5);
-insert into participante (id_legajo, nombre_apellido, id_proyecto)
-values (6,"antonio martinez",6);
-insert into participante (id_legajo, nombre_apellido, id_proyecto)
-values (7,"julieta cerreti",7);
-insert into participante (id_legajo, nombre_apellido, id_proyecto)
-values (8,"abigail gomez",8);
-insert into participante (id_legajo, nombre_apellido, id_proyecto)
-values (9,"selena solerinni",9);
-insert into participante (id_legajo, nombre_apellido, id_proyecto)
-values (10,"franco perez",10);
-insert into participante (id_legajo, nombre_apellido, id_proyecto)
-values (11,"carlos regetti",1);
-insert into participante (id_legajo, nombre_apellido, id_proyecto)
-values (12,"paula iriani",2);
+insert into participante (id_legajo, nombre_apellido,rol, id_proyecto)
+values (1,"juan gomez","admin",1);
+insert into participante (id_legajo, nombre_apellido,rol, id_proyecto)
+values (2,"marcelo centeno","ceo",2);
+insert into participante (id_legajo, nombre_apellido,rol, id_proyecto)
+values (3,"roberto alvarez","desarrollador",3);
+insert into participante (id_legajo, nombre_apellido,rol, id_proyecto)
+values (4,"clara perez","manager",4);
+insert into participante (id_legajo, nombre_apellido,rol, id_proyecto)
+values (5,"ana maria gutierrez","desarrollador",5);
+insert into participante (id_legajo, nombre_apellido,rol, id_proyecto)
+values (6,"antonio martinez","admin",6);
+insert into participante (id_legajo, nombre_apellido,rol, id_proyecto)
+values (7,"julieta cerreti","desarrollador",7);
+insert into participante (id_legajo, nombre_apellido,rol, id_proyecto)
+values (8,"abigail gomez","manager",8);
+insert into participante (id_legajo, nombre_apellido,rol, id_proyecto)
+values (9,"selena solerinni","programador",9);
+insert into participante (id_legajo, nombre_apellido,rol, id_proyecto)
+values (10,"franco perez","programador",10);
+insert into participante (id_legajo, nombre_apellido,rol, id_proyecto)
+values (11,"carlos regetti","manager",1);
+insert into participante (id_legajo, nombre_apellido,rol, id_proyecto)
+values (12,"paula iriani","desarrollador",2);
