de594757000620930ce999d30d044b20578b50c4
lgaia <112736144+lgaia@users.noreply.github.com>
2022-09-05 09:17:22 -0300

    Update CREACION DE TABLAS.sql

--- DDL/CREACION DE TABLAS.sql
+++ DDL/CREACION DE TABLAS.sql
@@ -1,21 +1,27 @@
 create database bdd2;
 use bdd2;
 create table cliente (
-id_cliente int not null auto_increment primary key,
-costos int not null, 
-facturacion int not null);
+  id_cliente int auto_increment,
+  hora_mensual int,
+  centro_costo int,
+  centro_facturacion int,
+  primary key(id_cliente, hora_mensual)
+);
+create table liquidacion (
+  hora_proyecto int primary key,
+  hora_cliente int,
+  constraint fk_cliente_liquidacion foreign key (hora_cliente) references cliente(hora_mensual)
+);
 create table proyecto (
-id_proyecto int not null auto_increment primary key,
-hora_mensual int not null,
-id_cliente int not null auto_increment,
-constraint fk_cliente_proyecto foreign key (id_cliente) references cliente (id_cliente));
+  id_proyecto int auto_increment primary key,
+  nombre_proyecto varchar(255),
+  hora_mensual int,
+  constraint fk_proyecto_liquidacion foreign key (hora_mensual) references liquidacion(hora_proyecto)
+);
 create table participante (
-id_legajo int not null auto_increment primary key,
-hora_max int not null,
-rol varchar(100) not null,
-id_proyecto int not null auto_increment,
-constraint fk_participante_proyecto foreign key (id_proyecto) references proyecto(id_proyecto));
-alter table proyecto
-add nombre_proyecto varchar(150);
-alter table participante
-add column nombre_apellido varchar(150) not null;
+  id_legajo int autoincrement primary key,
+  nombre_apellido varchar(150),
+  hora_diaria int,
+  id_proyecto int,
+  constraint fk_participante_proyecto foreign key (id_proyecto) references proyecto (id_proyecto)
+);
