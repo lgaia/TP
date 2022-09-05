3ecb52a645178a402cab73865774b2ebefe3057d
lgaia <112736144+lgaia@users.noreply.github.com>
2022-09-05 09:23:52 -0300

    Update TABLA CLIENTE.sql

--- DDL/TABLA CLIENTE.sql
+++ DDL/TABLA CLIENTE.sql
@@ -1,12 +1,21 @@
 use bdd2;
-insert into cliente (id_cliente, costos, facturacion)
-values (1,1000,1500);
-insert into cliente (costos, facturacion)
-values (500,1000);
-insert into cliente (costos, facturacion)
-values (750,1000);
-insert into cliente (costos, facturacion)
-values (250,500);
-insert into cliente (costos, facturacion)
-values (1000,2000);
-
+insert into cliente (id_cliente,hora_mensual,centro_costo,centro_facturacion)
+values (1,160,1000,1500);
+insert into cliente (hora_mensual, centro_costo, centro_facturacion)
+values(200,2500,5000);
+insert into cliente (hora_mensual, centro_costo, centro_facturacion)
+values(100,1500,3500);
+insert into cliente (hora_mensual, centro_costo, centro_facturacion)
+values(80,500,2000);
+insert into cliente (hora_mensual, centro_costo, centro_facturacion)
+values(120,1750,4250);
+insert into cliente (hora_mensual, centro_costo, centro_facturacion)
+values(140,750,1500);
+insert into cliente (hora_mensual, centro_costo, centro_facturacion)
+values(180,1500,2500);
+insert into cliente (hora_mensual, centro_costo, centro_facturacion)
+values(220,2000,4500);
+insert into cliente (hora_mensual, centro_costo, centro_facturacion)
+values(60,1000,6000);
+insert into cliente (hora_mensual, centro_costo, centro_facturacion)
+values(240,3500,10000);
