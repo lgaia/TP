2ea154c3644e24c8371272fed21b601ae7da805d
lgaia <112736144+lgaia@users.noreply.github.com>
2022-09-05 20:59:14 -0300

    Update RendicionDeHoras.sql

--- DESARROLLO/RendicionDeHoras.sql
+++ DESARROLLO/RendicionDeHoras.sql
@@ -6,6 +6,9 @@ INSERT INTO liquidacion (horas_proyecto, horas_cliente)
 SELECT SUM(horas_diarias)
 FROM participante
 WHERE id_proyecto=@idproyecto AND mes=@mes
+UPDATE TABLE cliente
+SET costo=100*horas_cliente
+WHERE id_proyecto=@idproyecto
 end
 $$
 DELIMITER;
