5a4277baec052dfde6ff23de0e5d4e31af93d4e3
lgaia <112736144+lgaia@users.noreply.github.com>
2022-09-05 20:54:05 -0300

    Update RendicionDeHoras.sql

--- DESARROLLO/RendicionDeHoras.sql
+++ DESARROLLO/RendicionDeHoras.sql
@@ -1,11 +1,11 @@
 use bdd2;
 DELIMITER $$
-create procedure RendicionDeHoras(IN idproyecto int, )
+create procedure RendicionDeHoras(IN idproyecto int, IN mes date)
 begin
 INSERT INTO liquidacion (horas_proyecto, horas_cliente)
 SELECT SUM(horas_diarias)
 FROM participante
-WHERE id_proyecto=@idproyecto
+WHERE id_proyecto=@idproyecto AND mes=@mes
 end
 $$
 DELIMITER;
