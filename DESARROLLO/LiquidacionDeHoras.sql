7adc192f0787b8bb446eeb90fbed510edad6ab22
lgaia <112736144+lgaia@users.noreply.github.com>
2022-09-12 20:51:50 -0300

    Update and rename RendicionDeHoras.sql to LiquidacionDeHoras.sql

---
+++ DESARROLLO/LiquidacionDeHoras.sql
@@ -0,0 +1,12 @@
+use bdd2;
+DELIMITER $$
+create procedure LiquidacionDeHoras(in ingresomes int)
+begin
+insert into liquidacion (id_proyecto, horas_proyecto, rol, mes)
+select id_proyecto, sum(cant_horas) as 'hora mensual', rol, month(fecha)
+from registrohora
+where month(fecha)=ingresomes
+group by id_proyecto and rol
+end
+$$
+
