e14146ca04e5fde883721831195e54aeb3e3a839
lgaia <112736144+lgaia@users.noreply.github.com>
2022-09-25 21:26:55 -0300

    Create TriggerProyecto.sql

---
+++ DESARROLLO/TriggerProyecto.sql
@@ -0,0 +1,24 @@
+use bdd2;
+DELIMITER $$
+create trigger proyecto_bi before insert on proyecto for each row 
+begin
+set @mi_tabla='proyecto';
+set @hay_dato=0;
+call numeradorCero(@mi_tabla,@hay_dato);
+if(select @hay_dato !=0) then
+begin
+insert into proyecto(id_proyecto)
+select siguiente
+from numerador
+where numerador.tabla='proyecto';
+update table numerador
+set siguiente=siguiente+1
+where numerador.tabla='proyecto';
+end;
+else
+insert into numerador(tabla, siguiente)
+values('proyecto',1);
+end if;
+end;
+$$
+DELIMITER ;
