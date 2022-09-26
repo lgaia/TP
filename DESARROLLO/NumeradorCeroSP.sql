9455c88a8c3e2f285c7f2840c620016593d8ceb4
lgaia <112736144+lgaia@users.noreply.github.com>
2022-09-25 21:24:18 -0300

    Create NumeradorCeroSP.sql

---
+++ DESARROLLO/NumeradorCeroSP.sql
@@ -0,0 +1,10 @@
+use bdd2;
+DELIMITER $$
+create procedure numeradorCero (in nombretabla varchar(255), out respuesta int)
+begin
+select count(tabla) into @respuesta
+from numerador
+where tabla=nombretabla
+end
+$$
+DELIMITER ;
