f555ac229ce4c7a6749ef3c2136de84807bbe53f
lgaia <112736144+lgaia@users.noreply.github.com>
2022-09-25 21:24:24 -0300

    Update TriggerCliente.sql

--- DESARROLLO/TriggerCliente.sql
+++ DESARROLLO/TriggerCliente.sql
@@ -1,5 +1,11 @@
+use bdd2;
 DELIMITER $$
 create trigger cliente_bi before insert on cliente for each row 
+begin
+set @mi_tabla='cliente';
+set @hay_dato=0;
+call numeradorCero(@mi_tabla,@hay_dato);
+if(select @hay_dato !=0) then
 begin
 insert into cliente(id_cliente)
 select siguiente
@@ -8,6 +14,11 @@ where numerador.tabla="cliente";
 update table numerador
 set siguiente=siguiente+1
 where numerador.tabla="cliente";
+end;
+else
+insert into numerador(tabla, siguiente)
+values("cliente",1);
+end if;
 end;
 $$
 DELIMITER ;
