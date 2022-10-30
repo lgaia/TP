4525f406a8159896e034970e4dd8d81b1b490fad
lgaia <112736144+lgaia@users.noreply.github.com>
2022-10-30 17:28:28 -0300

    Update ModificarTablas.sql

--- DDL/ModificarTablas.sql
+++ DDL/ModificarTablas.sql
@@ -1,14 +1,36 @@
 use bdd2;
+alter table proyecto
+drop constraint fk_proyecto_cliente;
+alter table participante
+drop constraint fk_participante_proyecto;
+alter table registrohora
+drop constraint fk_registrohora_participante;
+
 alter table cliente
-alter column id_cliente int not null primary key;
+drop primary key,
+change id_cliente id_cliente int not null,
+add primary key (id_cliente);
 
 alter table proyecto
-alter column id_proyecto int not null primary key;
+drop primary key,
+change id_proyecto id_proyecto int not null,
+add primary key (id_proyecto),
+add constraint fk_proyecto_cliente foreign key (id_cliente) 
+references cliente(id_cliente);
 
-alter table legajo
-alter column id_legajo int not null primary key;
+alter table participante
+drop primary key,
+change id_legajo id_legajo int not null,
+add primary key (id_legajo),
+add constraint fk_participante_proyecto foreign key (id_proyecto) 
+references proyecto(id_proyecto);
 
-create table numerador {
+alter table registrohora
+add constraint fk_registrohora_participante foreign key (id_legajo)
+references participante(id_legajo);
+
+create table numerador (
 tabla varchar(255) not null primary key,
 siguiente int
-};
+);
+
