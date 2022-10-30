17e5ab0bda8710d58287c6f0fa1e871f58f49802
lgaia <112736144+lgaia@users.noreply.github.com>
2022-10-30 18:07:57 -0300

    Update cargaDeDatosMensual.sql

--- DESARROLLO/cargaDeDatosMensual.sql
+++ DESARROLLO/cargaDeDatosMensual.sql
@@ -1,17 +1,10 @@
+use bdd2;
 DELIMITER $$
 CREATE PROCEDURE cargaDeDatos(IN idlegajo int, IN rol varchar(255), IN id_proyecto int, IN fecha date, IN canthora int)
 begin
 set @mes= MONTH(@fecha);
 insert into registrohora(id_legajo,rol,id_proyecto,canthora,fecha)
-values(@idlegajo,@rol,@idproyecto,@canthora,@fecha);
-set @dia = DATEADD(weekday,1,@fecha);
-while @mes = MONTH(@dia) do
-begin
-insert into registrohora(id_legajo,rol,id_proyecto,canthora,fecha)
-values(@idlegajo,@rol,@idproyecto,@canthora,@dia);
-set @dia = DATEADD(weekday,1,@dia);
-end;
-end while;
+values(@idlegajo,@rol,@idproyecto,@canthora,@mes);
 end;
 $$
 DELIMITER ;
